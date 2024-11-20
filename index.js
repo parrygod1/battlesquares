413ec6846083cd35af189f1e52fb4b00ad9fd9bb
Tudor Paraschiv <tudor.paraschiv@vastvisibility.co.uk>
2024-11-20 09:59:20 +0200

    add connect

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("asdfg")
+const axios = require("axios").default;
+
+const playerName = "5Leiz";
+const connectURL = "https://battlesquares.internal-tools.vastvisibility.co.uk/info/all";
+
+async function connect(gameId) {
+    const response = await axios.get(connectURL, {
+        params: {
+            gameId: gameId,
+            playerName: playerName
+        }
+    });
+    return response.data[0];
+}
+
+(async () => {
+    try {
+        const asdf = await connect(20);
+        console.log(asdf);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+})();
